3f8f0ce5edb99c6ec8cafb4f820e013f2bbf16e5
Ernst Peter Tamminga <ernst.peter.tamminga@xcess.nl>
2015-05-30 09:43:09 +0200

    Closes #1 some file update for showing Pull request

--- ContactOfficers.ascx.cs
+++ ContactOfficers.ascx.cs
@@ -1,6 +1,6 @@
 //
-// DotNetNuke® - http://www.dotnetnuke.com
-// Copyright (c) 2002-2010
+// DotNetNuke® - http://www.dnnsoftware.com
+// Copyright (c) 2002-2015
 // by DotNetNuke Corporation
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
@@ -11,7 +11,7 @@
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions 
 // of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// THE SOFTWARE IS PROVIDED "AS IS", 5ITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
 // TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
 // THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
 // CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
@@ -28,9 +28,9 @@ using DotNetNuke.Common;
 namespace DotNetNuke.Modules.UserGroups
 {
 
-	/// <summary>
-	/// 
-	/// </summary>
+    /// <summary>
+    /// Class ContactOfficers.
+    /// </summary>
     public partial class ContactOfficers : UserGroupsModuleBase 
     {
 
